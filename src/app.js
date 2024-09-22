f7694ee45bdf91f603729e5fb816a3bbfdf59def
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2024-09-22 10:12:23 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -3,12 +3,24 @@ const express = require("express");
 const app = express();
 
 app.use("/test", (req, res) => {
-  res.send("test from Server->Rajesh");
+  res.send("TEST from Server->Rajesh");
+});
+app.use("/user", (req, res) => {
+  res.send("TEST from Server->Rajesh");
 });
 
-app.use("/hello", (req, res) => {
-  res.send("Hello from hello route");
+app.get("/user", (req, res) => {
+  res.send({ name: "rajesh", role: "React" });
+});
+
+app.post("/user", (req, res) => {
+  //console.log("Save data to the Database");
+  res.send("Data successfully saved to the database");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully");
 });
 app.listen(3001, () => {
-  console.log("SERVER from 3000");
+  console.log("Server from 3000");
 });
