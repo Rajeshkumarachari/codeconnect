17ad573b752daef14e018af772afee1a728a63b9
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2024-09-22 10:28:32 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -2,25 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("TEST from Server->Rajesh");
-});
-app.use("/user", (req, res) => {
-  res.send("TEST from Server->Rajesh");
-});
-
-app.get("/user", (req, res) => {
+app.get("/user/:userid/:name/:password", (req, res) => {
+  console.log(req.params);
   res.send({ name: "rajesh", role: "React" });
 });
 
-app.post("/user", (req, res) => {
-  //console.log("Save data to the Database");
-  res.send("Data successfully saved to the database");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Deleted successfully");
-});
 app.listen(3001, () => {
   console.log("Server from 3000");
 });
